ec91bfafd5c053935928ff2df4d52d912197cba0
s3722279 <48265234+s3722279@users.noreply.github.com>
2020-05-12 12:16:59 +1000

    Add files via upload

---
+++ addlocation.php
@@ -0,0 +1,64 @@
+<?php
+session_start();
+//require_once './database.php';
+
+
+    //php not accurate or fully completed as database is not integrated 
+
+
+/*
+    
+if(isset($_POST['locationname']) && isset($_POST['xcoordinate']) && isset($_POST['ycoordinate']) && isset($_POST['locationtime']) && isset($_POST['description'])){
+    $locationname= $_POST['locationname'];
+    $xcoordinate= $_POST['xcoordinate'];
+    $ycoordinate= $_POST['ycoordinate'];
+    $locationtime= $_POST['locationtime'];
+    $description= $_POST['description'];
+
+    //check if duplicate location name exists in the database
+     if(createLocation($locationname)){
+        echo "Successfully created new location with the name of: $locationname"; 
+     }else{
+        echo "Error: Failed to create user. An location with the name of $locationname already exists."; 
+     }
+     
+
+   //if user pressed "Create new location" button without inputing all valid fields then they are warned.
+  }else if(isset($_POST['pressedAdd']) && !empty($_POST['pressedAdd'])){
+        
+    echo 'All fields must be entered';
+    unset($_POST['pressedAdd']);
+
+  }
+
+*/
+
+?>
+
+
+<main>
+    <h1> Add new location </h1>
+    
+
+
+
+
+    <script type="text/javascript">
+   
+
+    </script>
+
+    <form name="addlocation" method="post">
+    <p>Location Name:<input type="text" id="locationname" name="locationame"></p>
+    <p>X Coordinate:<input type="text" id="xcoordinate" name="xcoordinate"></p>
+    <p>Y Coordinate:<input type="text" id="ycoordinate" name="ycoordinate"></p>
+    <p>Location Time (min):<input type="number" id="locationtime" name="locationtime" ></p>
+    <label for="description">Description:</label>
+    <textarea id="description" name="description" rows="4" cols="40"> </textarea>
+    <br> <br>
+    <input type="hidden" id="pressedAdd" name="pressedAdd" value="1">
+    <a><input type = "submit" value="Create new location"><a>
+    <a href= "main.php"> Back <a>
+    </form>
+
+</main>
---
+++ removelocation.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+
+
+?>
+
+
+
+<main>
+    <h1> Remove location </h1>
+     <!-- Popup message confirming with user if they really want to delete the location selected -->
+    <script type="text/javascript">
+     
+    function confirmation() 
+        {
+            if(window.confirm("Do you really want to delete this location?")) 
+            { 
+               // Delete from database
+            }else{
+               // document.getElementById('pressedRegister').innerHTML = 0;
+               // document.getElementById('pressedRegister').value = 0;
+            }
+            
+        }
+
+
+    </script>
+     
+
+    <!-- Drop down list containing existing locations   -->
+    <form name="showlocation" method="post">
+    <select name= "locations" size ="1">
+        <option> Select Location</option>
+        <option value = "location1"> Location 1 </option>
+        <option value = "location2"> Location 2 </option>
+        <option value = "location3"> Location 3 </option>
+        <option value = "location4"> Location 4 </option>
+    </select>
+    <a><input type = "submit" value="Show"><a>
+    </form>
+
+
+
+    <form name = "removelocation" onsubmit= confirmation() method="post">
+    <p>Location Name:<input type="text" id="locationname" name="locationame"></p>
+    <p>X Coordinate:<input type="text" id="xcoordinate" name="xcoordinate"></p>
+    <p>Y Coordinate:<input type="text" id="ycoordinate" name="ycoordinate"></p>
+    <p>Location Time (min):<input type="number" id="locationtime" name="locationtime" ></p>
+    <label for="description">Description:</label>
+    <textarea id="description" name="description" rows="4" cols="40"> </textarea>
+    <br> <br>
+    <input type="hidden" id="pressedAdd" name="pressedAdd" value="1">
+    <a><input type = "submit" value="Delete Location"><a>
+    <a href= "main.php"> Back <a>
+    </form>
+
+
+
+
+
+</main>
