6d10bb855570c0bc42be1a4b6e11e9204bef4c07
s3660457 <s3660457@student.rmit.edu.au>
2020-05-13 16:24:41 +1000

    update edit_user.php

--- edit_user.php
+++ edit_user.php
@@ -1,28 +1,29 @@
-<?php
-
-?>
-
 <main>
     <head>
         <link rel="stylesheet" href="styles.css">
         <title>Edit User</title>
     </head>
     <script type="text/javascript">
-        //use SHA256 to hash password before sending to server.
         function editUser() {
             
         }
     </script>
     <h2>Edit User</h2>
-    <form name="user-editForm" onsubmit="return hashPass()" method="post" >
+    <form name="user-editForm" onsubmit="return editUser()" method="post" >
     <label for="users">Select User:</label>
     <select id="users">
-        <option value="user">Placeholder</option>
+        <?php
+            $query = mysql_query("SELECT username FROM users");
+            while($rowtwo = mysql_fetch_array($query)){
+            }
+        ?>
+        <option name="users" value=<?php echo $row["id"];?>><?php echo $row["id"];?></option>
     </select>
         <p>UserID: <input type="text" id="regUserID" name="regUserID"></p>
         <p>Password: <input type="password" id="regPassword" name="regPassword"></p><br>
         <input type="hidden" id="pressedRegister" name="pressedRegister" value="1">
         <a><input type="submit" value="Submit"></a>
+        <a href="login.php">Back<a>
     </form>
 
 </main>
