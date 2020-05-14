8f2f44bc2d3ebf38c6114df7e95a8ead2e12effe
s3660457 <s3660457@student.rmit.edu.au>
2020-05-14 15:26:19 +1000

    created links for menu

--- main.php
+++ main.php
@@ -19,11 +19,31 @@ session_start();
   
 
 ?>
-
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Main Menu</title>
+  </head>
 <body>
     <form name="logoutForm" method="post">
+        <h2>Main Menu</h2>
+        <feildset class ='login_form'>
+        <div class="row">
+            <a href="newuser.php"> 1. Create New User<a>
+        </div>
+        <div class="row">
+        <a href="edit_user.php"> 2. Edit User<a>
+        </div>
+        <div class="row">
+        <a href="addlocation.php"> 3. Add Location<a>
+        </div>
+        <div class="row">
+        <a href="editlocation.php"> 4. Edit Location<a>
+        </div>
+        <div class="row">
+        <a href="removelocation.php"> 5. Remove Location<a>
+        </div>
+        <br></br>
         <input type="hidden" id="Logout" name="Logout" value="1">
         <a><input type="submit" value="Logout"></a>
-        <a href="newuser.php">Create New User<a>
     </form>
 </body>
