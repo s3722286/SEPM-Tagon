828fbe420b6b86ba35771b710fcf7404a3a33057
Baptep <s3722286@student.rmit.edu.au>
2020-05-30 22:52:58 +1000

    Tour Types added to Create Tour and some cleanup

--- createtour.php
+++ createtour.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once './database.php';
 
 if(!isset($_SESSION['username'])){
         header("Location: login.php");
@@ -8,41 +9,51 @@ if(!isset($_SESSION['username'])){
 
 ?>
 
-<main>
-    <head>
-        <link rel="stylesheet" href="styles.css">
-        <title>Create New Tour</title>
-    </head>
-    <form name="createtour" method="post">
-      <h2> Create New Tour </h2>
-      <div class="row">
-        <p>Tour Name:<input type="text" id="tourname" name="tourname" required></p>
-      </div>
-      <!--Since a tour can have multiple locations, find a way to have the dropdown box duplicate itself if a location is selected?? -->
-      <div class="list">
+    <main>
+
+        <head>
+            <link rel="stylesheet" href="styles.css">
+            <title>Create New Tour</title>
+        </head>
+        <form name="createtour" method="post">
+            <h2> Create New Tour </h2>
             <div class="row">
-                <select name= "addlocations" size ="1">
+                <p>Tour Name:<input type="text" id="tourname" name="tourname" required></p>
+            </div>
+            <!--Since a tour can have multiple locations, find a way to have the dropdown box duplicate itself if a location is selected?? -->
+            <div class="list">
+                <div class="row">
+                    <label for="showtype">Add Location:</label>
+                    <select name="addlocations" size="1">
                     <option value="" disabled selected hidden>Add a location</option>
                 </select>
-                <a><input type = "submit" value="Add"><a>
-            </div>
-       
-        <div class="list">
-            <div class="row">
-                <select name= "showtype" size ="1">
+                </div>
+
+                <div class="list">
+                    <div class="row">
+                        <label for="showtype">Type of Tour:</label>
+                        <select name="showtype" size="1">
                     <option value="" disabled selected hidden>Add a tour type</option>
+                    
+                    <?php
+                    $row = getAllTourTypes();
+                        foreach($row as $next) {
+                            echo "<option value='".$next['tourTypeName']."'>".$next['tourTypeName']."</option>";
+                        }
+                    ?>
+                    
                 </select>
-                <a><input type = "submit" value="Add"><a>
+                    </div>
+                    <!-- Automatically calculates the time of all locations added up
+                    <div class="row">
+                        <p>Duration:<input type="number" id="duration" name="duration" required></p>
+                    </div> -->
+
+                    <div class="row">
+                        <input type="submit" value="Create new tour">
+                        <a href="main.php"> Back </a>
+                    </div>
+                </div>
             </div>
-     <!-- Automatically calculates the time of all locations added up -->
-      <div class="row">
-        <p>Duration:<input type="number" id="duration" name="duration" required></p>
-      </div>
-      
-      <div class="row">
-        <input type="hidden" id="pressedAdd" name="pressedAdd" value="1">
-        <a><input type = "submit" value="Create new tour"><a>
-        <a href= "main.php"> Back <a>
-      </div>
-    </form>
-</main>
+        </form>
+    </main>
